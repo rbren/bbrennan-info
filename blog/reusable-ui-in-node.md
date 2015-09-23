6fa5d2db10b5577457f9a3db2b2462c0e4b26d47
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-23 17:27:00 +0000

    move articles to gitback

--- blog/reusable-ui-in-node.md
+++
@@ -1,8 +0,0 @@
-{
-  "title": "Creating Reusable UI components in NodeJS",
-  "description": "A tutorial",
-  "date": "9-22-2015"
-}
-END_METADATA
-
-Often we have to reuse components.
--- routes/blog.js
+++ routes/blog.js
@@ -6,7 +6,7 @@ var Gitback = require('gitback');
 var BLOG_DIR = __dirname + '/../blog';
 var DATE_FORMAT = 'dddd, mmmm dS, yyyy';
 
-var ENTRIES = null;
+var ENTRIES = [];
 var ENTRIES_KEYED = {};
 
 var DB = new Gitback({
@@ -19,41 +19,12 @@ DB.initialize(function(err) {
   Router.use('/api', DB.router);
 })
 
-var reloadEntries = function() {
-  ENTRIES = FS.readdirSync(BLOG_DIR).filter(function(file) {
-    return file.indexOf('.') !== 0;
-  }).map(function(file) {
-    var entry = FS.readFileSync(BLOG_DIR + '/' + file, 'utf8')
-    var metadata = entry.substring(0, entry.indexOf('END_METADATA'));
-    metadata = JSON.parse(metadata);
-    metadata.contents = entry.substring(entry.indexOf('END_METADATA') + 12);
-    metadata.date = Date.parse(metadata.date);
-    metadata.dateString = DateFormat(metadata.date, DATE_FORMAT);
-    metadata.url = metadata.url || '/' + file.substring(0, file.lastIndexOf('.'));
-    return metadata;
-  }).sort(function(e1, e2) {
-    return e1.date > e2.date ? -1 : 
-           e1.date < e2.date ? 1 : 0;
-  });
-  ENTRIES.forEach(function(entry) {
-    ENTRIES_KEYED[entry.url.substring(1)] = entry;
-  })
-}
-reloadEntries();
-
-if (process.env.DEVELOPMENT) {
-  Router.use(function(req, res, next) {
-    reloadEntries();
-    next();
-  })
-}
-
 Router.get('/:post', function(req, res) {
-  var entry = ENTRIES_KEYED[req.params.post];
+  var entry = DB.collections.articles.get(req.params.post);
   if (!entry) return res.status(404).end();
   res.render('blog-post', {entry: entry});
 }) 
 
 Router.get('/', function(req, res) {
-  res.render('blog', {entries: ENTRIES});
+  res.render('blog', {entries: DB.collections.articles.get()});
 });
