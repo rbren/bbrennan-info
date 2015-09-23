b5dc2cf4748d75d2cc08cc4f52c0ba9912e8a1ad
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-23 02:05:48 +0000

    comment ui

--- static/js/blog-post.js
+++ static/js/blog-post.js
@@ -1,4 +1,4 @@
-var App = angular.module('app', []);
+var App = angular.module('app', ['hc.marked']);
 
 App.controller('Article', function($scope) {
   $scope.article = ARTICLE;
@@ -9,6 +9,14 @@ App.controller('Comments', function($scope) {
     $.getJSON('/blog/api/comments?article=' + encodeURIComponent($scope.article.url), function(comments) {
       console.log('com', comments);
       $scope.comments = comments;
+      $scope.comments.forEach(function(comment) {
+        comment.date = parseInt(comment.id);
+      })
+      $scope.comments.sort(function(c1, c2) {
+        if (c1.date > c2.date) return -1;
+        if (c1.date < c2.date) return 1;
+        return 0;
+      })
       $scope.$apply();
     });
   }
@@ -17,10 +25,14 @@ App.controller('Comments', function($scope) {
 
 App.controller('AddComment', function($scope) {
   $scope.addComment = function() {
+    $scope.sending = true;
     $.ajax('/blog/api/comments', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      success: $scope.refresh,
+      success: function() {
+        $scope.sending = false;
+        $scope.refresh();
+      },
       data: JSON.stringify({
         id: new String((new Date()).getTime()),
         article: $scope.article.url,
