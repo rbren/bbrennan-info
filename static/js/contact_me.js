1d0b895fbf15dfbb176b30e4238376cf740ebad6
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-29 22:18:53 +0000

    fix contact form, update portfolio

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "home/contact",
+                url: "/contact",
                 type: "POST",
                 data: JSON.stringify({
                     name: name,
